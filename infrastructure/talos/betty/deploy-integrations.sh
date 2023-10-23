9999b9ac395b552defa05ed2ad841c32e0d4c84c
Stephen Beale <stephen.r.beale@gmail.com>
2023-10-23 14:06:47 -0600

    feat(talos): :wrench: Move to KubePrizm

--- infrastructure/talos/betty/deploy-integrations.sh
+++ infrastructure/talos/betty/deploy-integrations.sh
@@ -1,21 +1,42 @@
 #!/usr/bin/env bash
 # shellcheck disable=2312
 
-git_repo_url="git@github.com:prometheus-operator/prometheus-operator.git"
-yaml_dir="example/prometheus-operator-crd"
+# Git URLs of the repositories and directories
+declare -A repos_and_dirs=(
+    ["git@github.com:prometheus-operator/prometheus-operator.git"]="example/prometheus-operator-crd"
+    ["git@github.com:kubernetes-sigs/gateway-api.git"]="config/crd/standard"
+)
+
+# File to retrieve from the experimental directory
+experimental_file="git@github.com:kubernetes-sigs/gateway-api.git:config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml"
 
 pushd integrations >/dev/null 2>&1 || exit 1
 
-tmp_dir=$(mktemp -d)
-git clone "$git_repo_url" "$tmp_dir"
+# Loop through the repositories and directories
+for repo_url in "${!repos_and_dirs[@]}"; do
+    repo_dir="${repos_and_dirs[$repo_url]}"
 
-cd "$tmp_dir/$yaml_dir"
+    # Clone the Git repository to a temporary directory using SSH
+    tmp_dir=$(mktemp -d)
+    git clone "$repo_url" "$tmp_dir"
 
-for file in *.yaml; do
-    kubectl apply -f "$file"
+    # Change to the specified directory within the repository
+    cd "$tmp_dir/$repo_dir"
+
+    # Loop through the YAML files in the directory and apply them
+    for file in *.yaml; do
+        kubectl apply -f "$file"
+    done
+
+    # Clean up the temporary directory
+    rm -rf "$tmp_dir"
 done
 
-rm -rf "$tmp_dir"
+# Retrieve the file from the experimental directory and apply it
+tmp_file=$(mktemp)
+git archive --remote="$experimental_file" HEAD "config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml" >"$tmp_file"
+kubectl apply -f "$tmp_file"
+rm "$tmp_file"
 
 rm -rf cni/charts
 envsubst <../../../../kubernetes/apps/kube-system/cilium/app/values.yaml >cni/values.yaml
@@ -28,3 +49,5 @@ envsubst <../../../../kubernetes/apps/kube-system/kubelet-csr-approver/app/value
 kustomize build --enable-helm kubelet-csr-approver | kubectl apply -f -
 rm kubelet-csr-approver/values.yaml
 rm -rf kubelet-csr-approver/charts
+
+popd >/dev/null 2>&1
