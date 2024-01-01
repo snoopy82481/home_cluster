1b6a40f8984504db06a24fb5b5e3f0cfd5c1153a
Stephen Beale <stephen.r.beale@gmail.com>
2023-12-31 20:00:10 -0700

    refactor(talos): Update talos scripts

--- infrastructure/talos/betty/deploy-integrations.sh
+++ infrastructure/talos/betty/deploy-integrations.sh
@@ -1,42 +1,40 @@
-#!/usr/bin/env bash
-# shellcheck disable=2312
+#!/usr/bin/env fish
 
 # Git URLs of the repositories and directories
-declare -A repos_and_dirs=(
-    ["git@github.com:prometheus-operator/prometheus-operator.git"]="example/prometheus-operator-crd"
-    ["git@github.com:kubernetes-sigs/gateway-api.git"]="config/crd/standard"
-)
+set -l repos_and_dirs
+set repos_and_dirs[git@github.com:prometheus-operator/prometheus-operator.git] example/prometheus-operator-crd
+set repos_and_dirs[git@github.com:kubernetes-sigs/gateway-api.git] config/crd/standard
 
 # File to retrieve from the experimental directory
-experimental_file="git@github.com:kubernetes-sigs/gateway-api.git:config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml"
+set experimental_file git@github.com:kubernetes-sigs/gateway-api.git:config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
 
 pushd integrations >/dev/null 2>&1 || exit 1
 
 # Loop through the repositories and directories
-for repo_url in "${!repos_and_dirs[@]}"; do
-    repo_dir="${repos_and_dirs[$repo_url]}"
+for repo_url in $repos_and_dirs
+    set repo_dir $repos_and_dirs[$repo_url]
 
     # Clone the Git repository to a temporary directory using SSH
-    tmp_dir=$(mktemp -d)
-    git clone "$repo_url" "$tmp_dir"
+    set tmp_dir (mktemp -d)
+    git clone $repo_url $tmp_dir
 
     # Change to the specified directory within the repository
-    cd "$tmp_dir/$repo_dir"
+    cd $tmp_dir/$repo_dir
 
     # Loop through the YAML files in the directory and apply them
-    for file in *.yaml; do
-        kubectl apply -f "$file"
-    done
+    for file in *.yaml
+        kubectl apply -f $file
+    end
 
     # Clean up the temporary directory
-    rm -rf "$tmp_dir"
-done
+    rm -rf $tmp_dir
+end
 
 # Retrieve the file from the experimental directory and apply it
-tmp_file=$(mktemp)
-git archive --remote="$experimental_file" HEAD "config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml" >"$tmp_file"
-kubectl apply -f "$tmp_file"
-rm "$tmp_file"
+set tmp_file (mktemp)
+git archive --remote=$experimental_file HEAD config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml >$tmp_file
+kubectl apply -f $tmp_file
+rm $tmp_file
 
 rm -rf cni/charts
 envsubst <../../../../kubernetes/apps/kube-system/cilium/app/values.yaml >cni/values.yaml
