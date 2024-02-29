68d59d2846dc5193978e03fb1aa60be5025a744b
Stephen Beale <stephen.r.beale@gmail.com>
2024-02-29 15:41:32 -0700

    ci: Update .prettierignore and lint.yaml, add validate_kustomizations function

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -20,6 +20,19 @@ kubeconform_args=(
     "-verbose"
 )
 
+validate_kustomizations() {
+    local dir=$1
+    echo "=== Validating kustomizations in ${dir} ==="
+    find "${dir}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+        echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
+        kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
+            kubeconform "${kubeconform_args[@]}"
+        if [[ ${PIPESTATUS[0]} != 0 ]]; then
+            exit 1
+        fi
+    done
+}
+
 echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
     kubeconform "${kubeconform_args[@]}" "${file}"
@@ -28,22 +41,5 @@ find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while
     fi
 done
 
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
-find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
-    kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
-        kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        exit 1
-    fi
-done
-
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
-find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
-    kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
-        kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        exit 1
-    fi
-done
+validate_kustomizations "${KUBERNETES_DIR}/flux"
+validate_kustomizations "${KUBERNETES_DIR}/apps"
